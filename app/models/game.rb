b0f2e91ad7e0dbadafc2b5d681d3dd7a6d884c4d
sekito <sekito1107gg@gmail.com>
2026-01-10 09:59:48 +0900

    lintによる修正

--- app/models/game.rb
+++ app/models/game.rb
@@ -3,15 +3,15 @@ class Game < ApplicationRecord
   enum :status, { matching: 0, playing: 1, finished: 2 }, default: :matching
 
   # 勝者・敗者
-  belongs_to :winner, class_name: 'User', optional: true
-  belongs_to :loser, class_name: 'User', optional: true
+  belongs_to :winner, class_name: "User", optional: true
+  belongs_to :loser, class_name: "User", optional: true
 
   # 参加プレイヤー
   has_many :game_players, dependent: :destroy
 
   # 参加ユーザー一覧
   has_many :users, through: :game_players
-  
+
   # 盤面のカードとターン履歴
   has_many :game_cards, dependent: :destroy
   has_many :turns, dependent: :destroy
