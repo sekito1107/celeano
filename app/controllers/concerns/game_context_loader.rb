c7f9d99c286d6e36ef026fe06ff5125adb081d90
sekito <sekito1107gg@gmail.com>
2026-01-18 14:22:11 +0900

    機能追加: ゲームコンテキスト読み込みの共通化

---
+++ app/controllers/concerns/game_context_loader.rb
@@ -0,0 +1,20 @@
+module GameContextLoader
+  extend ActiveSupport::Concern
+
+  included do
+    before_action :set_game_context
+  end
+
+  private
+
+  def set_game_context
+    @game = Game.find(params[:game_id])
+    @game_player = @game.game_players.find_by!(user: current_user)
+    @turn = @game.turns.order(turn_number: :desc).first
+  rescue ActiveRecord::RecordNotFound
+    respond_to do |format|
+      format.html { redirect_to lobby_path, alert: "ゲームまたはプレイヤーが見つかりません" }
+      format.json { render json: { status: "error", message: "Game or player not found" }, status: :not_found }
+    end
+  end
+end
